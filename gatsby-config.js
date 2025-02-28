0fdb2d482f2d63d07b8aee1a73a8a69981bdcae5
Asuki Yamashita <asuky.y@gmail.com>
2025-03-01 08:57:45 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mdx-poc`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
