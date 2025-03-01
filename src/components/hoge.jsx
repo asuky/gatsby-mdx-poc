a4e982de5015ec44ee65147cd15a5bca6322fa17
Asuki Yamashita <asuky.y@gmail.com>
2025-03-01 15:15:12 +0900

    Hoge コンポーネント作成、階層をつけても動作

---
+++ src/components/hoge.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Hoge = ({ children }) => {
+  return (
+    <>
+      <p>PiyoPiyo</p>
+      {children}
+      <p>Fugafuga</p>
+    </>
+  );
+};
+
+export default Hoge;
